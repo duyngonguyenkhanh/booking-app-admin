646c4dc0a68d7632fc776baf1147f0d5ab21b852
Khanh Duy <duyngonguyenkhanh@gmail.com>
2024-12-28 14:35:58 +0700

    fix package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/booking-app-admin/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['jwt-decode'],
